d462f01592712dd18413f7252b9c278a32201239
GiuTan <97614010+GiuTan@users.noreply.github.com>
2025-03-13 10:38:45 +0100

    errata corrige

--- models_NILM/CRNN/data/ukdale_parser.py
+++ models_NILM/CRNN/data/ukdale_parser.py
@@ -30,7 +30,7 @@ class UK_Dale_Parser:
         for house_id in house_indicies:
             house_data = pd.read_csv(os.path.join(self.data_location, f'nilm_solar_HOUSE_{house_id}_FINAL.csv'))
             house_data = house_data.set_index('Time')
-
+	    house_data = house_data[['active','kettle', 'microwave', 'fridge', 'washing machine', 'dish washer']]
             if house_id == house_indicies[0]:
                 entire_data = house_data
                 if len(house_indicies) == 1:
@@ -39,10 +39,8 @@ class UK_Dale_Parser:
                 entire_data = entire_data.append(house_data, ignore_index=True)
 
         entire_data = entire_data.dropna().copy()
-        # ritorna l'input che è il net load e l'output come lista di solar + nilm
-        return entire_data.values[:, -1:], [entire_data.values[:, 1:2], np.concatenate([entire_data.values[:, :1],entire_data.values[:,2:-1]], axis=1)]
-        #return entire_data.values[:, -1:], [entire_data.values[:, 1:2], entire_data.values[:, 2:-1]]
 
+        return entire_data.values[:, :1], entire_data.values[:, 1:]
 
 
     def get_datasets(self):
@@ -56,22 +54,17 @@ class UK_Dale_Parser:
         x_test = 2 * (self.x_test-self.x_min)/(self.x_max-self.x_min) - 1
 
         # Normalization of appliances in [0,1]
-        self.y_min_solar = np.min(self.y[0][val_end:], axis=0)
-        self.y_min_nilm = np.min(self.y[1][val_end:], axis=0)
-        self.y_max_solar = np.max(self.y[0][val_end:], axis=0)
-        self.y_max_nilm = np.max(self.y[1][val_end:], axis=0)
+        self.y_min = np.min(self.y[val_end:], axis=0)
+        self.y_max = np.max(self.y[val_end:], axis=0)
+        self.y = (self.y-self.y_min)/(self.y_max-self.y_min)
+        self.y_test = (self.y_test-self.y_min)/(self.y_max-self.y_min)
 
-        self.y[0] = (self.y[0] -self.y_min_solar)/(self.y_max_solar -self.y_min_solar)
-        self.y[1] = (self.y[1] - self.y_min_nilm) / (self.y_max_nilm - self.y_min_nilm)
-        self.y_test[0] = (self.y_test[0]-self.y_min_solar)/(self.y_max_solar-self.y_min_solar)
-        self.y_test[1] = (self.y_test[1] - self.y_min_nilm) / (self.y_max_nilm - self.y_min_nilm)
-
-        val = NILMDataset(x_val, [self.y[0][:val_end],self.y[1][:val_end]], self.seq_len, self.pred_len, self.seq_len)
+        val = NILMDataset(x_val, self.y[:val_end], self.seq_len, self.pred_len, self.seq_len)
 
         # overlapping patches for training
-        train = NILMDataset(x_train, [self.y[0][val_end:],self.y[1][val_end:]], self.seq_len, self.pred_len, self.window_stride)
+        train = NILMDataset(x_train, self.y[val_end:], self.seq_len, self.pred_len, self.window_stride)
 
         # no overlapping patches for testing
-        test = NILMDataset(x_test, [self.y_test[0],self.y_test[1]], self.seq_len, self.pred_len, self.seq_len)
+        test = NILMDataset(x_test, self.y_test, self.seq_len, self.pred_len, self.seq_len)
 
-        return train, val, test, self.x_min, self.x_max, [self.y_min_solar, self.y_min_nilm], [self.y_max_solar, self.y_max_nilm]
+        return train, val, test, self.x_min, self.x_max, self.y_min, self.y_max
